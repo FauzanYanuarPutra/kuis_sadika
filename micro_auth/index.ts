1a5857c09d41f4d126fd4cfddcc9385c489c05c8
fauzan <ccdplayerx@gmail.com>
2023-11-15 09:09:07 +0700

    micro_quiz

--- micro_auth/index.ts
+++ micro_auth/index.ts
@@ -20,28 +20,22 @@ const maxPeoplePerRoom = 5;
 let currentRoomNumber = 1;
 
 io.on("connection", (socket) => {
-  // Assign the user to a room
   const room = `room${currentRoomNumber}`;
   socket.join(room);
 
-  // Notify the user about their room
   socket.emit("room_assigned", { room });
 
-  // Increment the room number if the current room is full
   const roomSize = io.sockets.adapter.rooms.get(room).size;
   if (roomSize >= maxPeoplePerRoom) {
     currentRoomNumber++;
   }
 
-  // Broadcast to all clients in the room when a new user joins
   io.to(room).emit("user_joined", { username: "User" + socket.id });
 
-  // Handle chat messages
   socket.on("chat_message", (message) => {
     io.to(room).emit("chat_message", { username: "User" + socket.id, message });
   });
 
-  // Handle disconnection
   socket.on("disconnect", () => {
     io.to(room).emit("user_left", { username: "User" + socket.id });
   });
