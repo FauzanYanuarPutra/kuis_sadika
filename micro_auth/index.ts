e2540c5c38400315d62d5d93e55cce7fb61ba1a8
fauzan <ccdplayerx@gmail.com>
2023-11-15 10:51:13 +0700

    golang port

--- micro_auth/index.ts
+++ micro_auth/index.ts
@@ -1,46 +1,84 @@
+import { AppDataSource } from "./data-source";
 import { startGRPCServer } from "./grpcServer";
+import * as cors from 'cors'
 
 const express = require("express");
-const http = require("http");
-const socketIO = require("socket.io");
-
+// const http = require("http");
+// const socketIO = require("socket.io");
 const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
+// const server = http.createServer(app);
+// const io = socketIO(server);
 
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Socket.IO server is running.");
-});
+AppDataSource.initialize().then(async () => {
+    const app = express()
+    const port = 5000
 
-startGRPCServer();
+    app.use(cors())
+    app.use(express.json())
 
-const maxPeoplePerRoom = 5;
-let currentRoomNumber = 1;
+    app.get("/", (req, res) => {
+      res.send("Socket.IO server is running.");
+    });
 
-io.on("connection", (socket) => {
-  const room = `room${currentRoomNumber}`;
-  socket.join(room);
+    startGRPCServer();
 
-  socket.emit("room_assigned", { room });
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}).catch(error => console.log(error))
 
-  const roomSize = io.sockets.adapter.rooms.get(room).size;
-  if (roomSize >= maxPeoplePerRoom) {
-    currentRoomNumber++;
-  }
 
-  io.to(room).emit("user_joined", { username: "User" + socket.id });
+// const maxPeoplePerRoom = 5;
+// let rooms = {}; // Object to store rooms dynamically
 
-  socket.on("chat_message", (message) => {
-    io.to(room).emit("chat_message", { username: "User" + socket.id, message });
-  });
+// io.on("connection", (socket) => {
+//   let userRoom;
 
-  socket.on("disconnect", () => {
-    io.to(room).emit("user_left", { username: "User" + socket.id });
-  });
-});
+//   const joinRoom = () => {
+//     // Find or create a new room
+//     const availableRoom = Object.keys(rooms).find(
+//       (room) => rooms[room].length < maxPeoplePerRoom
+//     );
 
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+//     if (availableRoom) {
+//       userRoom = availableRoom;
+//     } else {
+//       userRoom = `room${Object.keys(rooms).length + 1}`;
+//       rooms[userRoom] = [];
+//     }
+
+//     socket.join(userRoom);
+//     rooms[userRoom].push(socket.id);
+
+//     socket.emit("room_assigned", { room: userRoom });
+
+//     io.to(userRoom).emit("user_joined", { username: "User" + socket.id });
+
+//     // Check if the room is full
+//     if (rooms[userRoom].length === maxPeoplePerRoom) {
+//       // Create a new room if needed
+//       userRoom = `room${Object.keys(rooms).length + 1}`;
+//       rooms[userRoom] = [];
+//     }
+//   };
+
+//   joinRoom();
+
+//   socket.on("chat_message", (message) => {
+//     io.to(userRoom).emit("chat_message", { username: "User" + socket.id, message });
+//   });
+
+//   socket.on("disconnect", () => {
+//     io.to(userRoom).emit("user_left", { username: "User" + socket.id });
+
+//     // Remove the user from the room
+//     rooms[userRoom] = rooms[userRoom].filter((id) => id !== socket.id);
+
+//     // If the room is empty, remove it
+//     if (rooms[userRoom].length === 0) {
+//       delete rooms[userRoom];
+//     }
+//   });
+// });
--- micro_auth/services/user/index.ts
+++ micro_auth/services/user/index.ts
@@ -1,19 +1,19 @@
 // userService.ts
 import * as grpc from "@grpc/grpc-js";
 import { loadSync } from "@grpc/proto-loader";
+import { AppDataSource } from "../../data-source";
+import { User } from "../../entity/User";
 
 const PROTO_PATH = "../user/user.proto";
 const packageDefinition = loadSync(PROTO_PATH);
 export const userProto: any = grpc.loadPackageDefinition(packageDefinition).user;
 
-interface User {
-  id: string;
+interface IUser {
+  id?: string;
   username: string;
   email: string;
-  profile_picture: string;
+  avatar: string;
 }
-
-
 
 export type CreateUserResponse = {
   success: boolean;
@@ -21,194 +21,65 @@ export type CreateUserResponse = {
 };
 
 
-const userData: User[] = [
-  {
-    id: "1",
-    username: "john_doe",
-    email: "john.doe@example.com",
-    profile_picture: "url_to_johns_picture",
-  },
-  {
-    id: "2",
-    username: "jane_doe",
-    email: "jane.doe@example.com",
-    profile_picture: "url_to_janes_picture",
-  },
-  {
-    id: "3",
-    username: "james_smith",
-    email: "james.smith@example.com",
-    profile_picture: "url_to_james_picture",
-  },
-  {
-    id: "4",
-    username: "emma_johnson",
-    email: "emma.johnson@example.com",
-    profile_picture: "url_to_emma_picture",
-  },
-  {
-    id: "5",
-    username: "alex_smith",
-    email: "alex.smith@example.com",
-    profile_picture: "url_to_alex_picture",
-  },
-  {
-    id: "6",
-    username: "sarah_johnson",
-    email: "sarah.johnson@example.com",
-    profile_picture: "url_to_sarah_picture",
-  },
-  {
-    id: "7",
-    username: "michael_brown",
-    email: "michael.brown@example.com",
-    profile_picture: "url_to_michael_picture",
-  },
-  {
-    id: "8",
-    username: "emily_davis",
-    email: "emily.davis@example.com",
-    profile_picture: "url_to_emily_picture",
-  },
-  {
-    id: "9",
-    username: "william_smith",
-    email: "william.smith@example.com",
-    profile_picture: "url_to_william_picture",
-  },
-  {
-    id: "10",
-    username: "olivia_johnson",
-    email: "olivia.johnson@example.com",
-    profile_picture: "url_to_olivia_picture",
-  },
-  {
-    id: "11",
-    username: "jacob_brown",
-    email: "jacob.brown@example.com",
-    profile_picture: "url_to_jacob_picture",
-  },
-  {
-    id: "12",
-    username: "ava_davis",
-    email: "ava.davis@example.com",
-    profile_picture: "url_to_ava_picture",
-  },
-  {
-    id: "13",
-    username: "noah_smith",
-    email: "noah.smith@example.com",
-    profile_picture: "url_to_noah_picture",
-  },
-  {
-    id: "14",
-    username: "isabella_johnson",
-    email: "isabella.johnson@example.com",
-    profile_picture: "url_to_isabella_picture",
-  },
-  {
-    id: "15",
-    username: "liam_brown",
-    email: "liam.brown@example.com",
-    profile_picture: "url_to_liam_picture",
-  },
-  {
-    id: "16",
-    username: "mia_davis",
-    email: "mia.davis@example.com",
-    profile_picture: "url_to_mia_picture",
-  },
-  {
-    id: "17",
-    username: "ethan_smith",
-    email: "ethan.smith@example.com",
-    profile_picture: "url_to_ethan_picture",
-  },
-  {
-    id: "18",
-    username: "ava_johnson",
-    email: "ava.johnson@example.com",
-    profile_picture: "url_to_ava_picture",
-  },
-  {
-    id: "19",
-    username: "logan_brown",
-    email: "logan.brown@example.com",
-    profile_picture: "url_to_logan_picture",
-  },
-  {
-    id: "20",
-    username: "harper_davis",
-    email: "harper.davis@example.com",
-    profile_picture: "url_to_harper_picture",
-  },
-  {
-    id: "21",
-    username: "aiden_smith",
-    email: "aiden.smith@example.com",
-    profile_picture: "url_to_aiden_picture",
-  },
-  {
-    id: "22",
-    username: "abigail_johnson",
-    email: "abigail.johnson@example.com",
-    profile_picture: "url_to_abigail_picture",
-  },
-  {
-    id: "23",
-    username: "carter_brown",
-    email: "carter.brown@example.com",
-    profile_picture: "url_to_carter_picture",
-  },
-  {
-    id: "24",
-    username: "elizabeth_davis",
-    email: "elizabeth.davis@example.com",
-    profile_picture: "url_to_elizabeth_picture",
-  },
-  {
-    id: "25",
-    username: "mason_smith",
-    email: "mason.smith@example.com",
-    profile_picture: "url_to_mason_picture",
-  },
-];
-
 export const userService = {
-  GetUser: (
-    call: grpc.ServerUnaryCall<{ id: string }, User>, 
+  UserRepository: AppDataSource.getRepository(User),
+
+  GetUser: async (
+    call: grpc.ServerUnaryCall<{ id: string }, IUser>, 
     callback: grpc.sendUnaryData<User>
   ) => {
-    const id = call.request.id;
-    const user = userData.find((u) => u.id === id);
-    if (user) {
-      callback(null, user);
-    } else {
+    try {
+      const id = Number(call.request.id);
+      const user: any = await userService.UserRepository.findOne({
+        where: { id },
+      });
+
+      if (user) {
+        callback(null, user);
+      } else {
+        callback({
+          code: grpc.status.NOT_FOUND,
+          details: "User not found",
+        });
+      }
+    } catch (error) {
+      console.error("Error fetching user:", error);
       callback({
-        code: grpc.status.NOT_FOUND,
-        details: "User not found",
+        code: grpc.status.INTERNAL,
+        details: "Internal server error",
       });
     }
   },
 
-  CreateUser: (call: grpc.ServerUnaryCall<User, CreateUserResponse>, callback: grpc.sendUnaryData<CreateUserResponse>) => {
-    const newUser = {
-      id: "3",
-      username: call.request.username,
-      email: call.request.email,
-      profile_picture: call.request.profile_picture,
-    };
+  CreateUser: async (call: grpc.ServerUnaryCall<IUser, CreateUserResponse>, callback: grpc.sendUnaryData<CreateUserResponse>) => {
+    try {
 
-    userData.push(newUser);
+      const email: string = call.request.email;
+      const user: any = await userService.UserRepository.findOne({
+        where: { email },
+      });
 
-    const response: CreateUserResponse = {
-      success: true,
-      message: "User created successfully",
-    };
+      if (!user) {
+        const newUser = new User();
+        newUser.username = call.request.username;
+        newUser.email = call.request.email;
+        newUser.avatar = call.request.avatar;
+        await userService.UserRepository.save(newUser);
+      }
 
-    callback(null, response);
+      const response: CreateUserResponse = {
+        success: true,
+        message: "User created successfully",
+      };
+
+      callback(null, response);
+    } catch (error) {
+      console.error("Error creating user:", error);
+      callback({
+        code: grpc.status.INTERNAL,
+        details: "Internal server error",
+      });
+    }
   },
 
 };
-
-
